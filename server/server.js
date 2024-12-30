a083da7f02e605dc96aadfdf723ff115eb6ccc66
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-29 23:52:02 -0800

    ✨ feat: initialize Apollo Server with type definitions and resolvers
    - 🛠️ Configured `ApolloServer` with `typeDefs` and `resolvers` from the `schemas` module
    - 🔧 Imported `db` connection for integrating the server with the database

--- server/server.js
+++ server/server.js
@@ -4,7 +4,13 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
