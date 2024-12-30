ad9cdac1f14b6dfae9ded7c72be5908f32a481e8
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-29 23:22:34 -0800

    ✨ feat: add `saveBook` mutation for saving books to user profile
    - 🛠️ Implemented `saveBook` mutation to allow authenticated users to save book data to their profile
    - 🔒 Added authentication check to ensure only logged-in users can save books
    - 📚 Used MongoDB `$push` operator to add book data to the `savedBooks` array in the user model
    - 🚀 Returned the updated user document for immediate feedback on successful operation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -36,4 +36,20 @@ const resolvers = {
         const token = signToken(user);
         return { token, user };
       },
+
+      saveBook: async (parent, { bookData }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { savedBooks: bookData } },
+            { new: true }
+          );
+  
+          return updatedUser;
+        }
+  
+        throw AuthenticationError;
+      },
+
+
   }
