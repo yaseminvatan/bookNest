196592f5335a5f69e42e09f20e06d176ac786e4e
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-29 23:10:26 -0800

    ✨ feat: add `addUser` and `login` mutations for user authentication and registration
    - 🛠️ Implemented `addUser` mutation to create new users, generate a JWT token, and return user data
    - 🔑 Implemented `login` mutation to authenticate users by email and password
    - 🔒 Added validation to handle incorrect email or password and throw an `AuthenticationError`
    - 🚀 Enhanced GraphQL API to support secure user registration and login functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,3 +13,27 @@ const resolvers = {
       throw AuthenticationError;
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw AuthenticationError;
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw AuthenticationError;
+        }
+  
+        const token = signToken(user);
+        return { token, user };
+      },
+  }
