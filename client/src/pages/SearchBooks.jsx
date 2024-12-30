c18bed4caf03cb2b52b6cd7f65b2bc212007d916
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-29 22:16:31 -0800

    ♻️ refactor: transition from REST API to GraphQL for saving books
    - 🔄 Replaced `saveBook` REST API call with `SAVE_BOOK` GraphQL mutation
    - 🛠️ Integrated Apollo Client's `useMutation` hook for managing book-saving operations

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,7 +9,8 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
