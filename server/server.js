b1ed87d0c7d15c9d6f2d72e78666d1fd636cc9fa
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-30 00:08:02 -0800

    WIP: instance created with Graphql and Apollo Server

--- server/server.js
+++ server/server.js
@@ -14,16 +14,31 @@ const server = new ApolloServer({
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const startApolloServer = async () => {
+  await server.start();
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/graphql', expressMiddleware(server, {
+  context: authMiddleware
+}));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 API server running on port :${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  }
+  );
 });
+};
+
+startApolloServer();
