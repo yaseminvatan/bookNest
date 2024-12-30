d025efcbe83275716d439257f4348f0cb88c6e21
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-29 21:39:57 -0800

    ✨ feat: integrate error handling for user registration with GraphQL mutation
    - 🔄 Initialized `addUser` mutation using Apollo Client's `useMutation` hook
    - ⚠️ Added `useEffect` to dynamically handle and display errors by toggling an alert state
    - 🚀 Enhanced user experience with better feedback for registration errors

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -8,12 +8,24 @@ import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+     username: '', email: '', password: ''
+     });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  
+  const [addUser, { error }] = useMutation(ADD_USER);
 
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+ 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
