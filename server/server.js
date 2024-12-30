467d35f30c542d10e13d1dc6c0922256ed1ce871
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-29 23:46:54 -0800

    ✨ feat: set up Apollo Server and middleware for GraphQL API
    - 🛠️ Imported `ApolloServer` from `@apollo/server` to initialize the GraphQL server
    - 🔧 Added `expressMiddleware` from `@apollo/server/express4` to integrate Apollo Server with Express
    - 🔒 Incorporated `authMiddleware` for secure request handling by verifying JWT tokens
    - 🚀 Prepared foundational setup for building a robust and secure GraphQL API

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
